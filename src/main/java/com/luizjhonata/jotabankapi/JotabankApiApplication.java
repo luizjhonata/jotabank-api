3ac329f1fb978b220ec059eb478602057146df1c
Jhonata Luiz <jhonataluiz@hotmail.com>
2022-07-13 13:22:19 -0300

    project created

---
+++ src/main/java/com/luizjhonata/jotabankapi/JotabankApiApplication.java
@@ -0,0 +1,13 @@
+package com.luizjhonata.jotabankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotabankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotabankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizjhonata/jotabankapi/JotabankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizjhonata.jotabankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotabankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
